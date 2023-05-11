f21ece9c2c78bd460d42c7d135f02487b880a907
RagunathRa <ragunath065@gmail.com>
2023-05-11 21:14:10 +0530

    Calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [output, setOutput] = useState('');
+
+  const display = (num) => {
+    setOutput(output + num);
+  };
+
+  const calculate = () => {
+    try {
+      setOutput(eval(output).toString());
+    } catch (error) {
+      alert('Invalid expression');
+    }
+  };
+
+  const clear = () => {
+    setOutput('');
+  };
+
+  const del = () => {
+    setOutput(output.slice(0, -1));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="calculator">
+        <input type="text" placeholder="0" value={output} onChange={() => {}} />
+        <button onClick={clear}>cl</button>
+        <button onClick={del}>Del</button>
+        <button onClick={() => display('%')}>%</button>
+        <button onClick={() => display('/')}>/</button>
+        <button onClick={() => display('7')}>7</button>
+        <button onClick={() => display('8')}>8</button>
+        <button onClick={() => display('9')}>9</button>
+        <button onClick={() => display('*')}>*</button>
+        <button onClick={() => display('4')}>4</button>
+        <button onClick={() => display('5')}>5</button>
+        <button onClick={() => display('6')}>6</button>
+        <button onClick={() => display('-')}>-</button>
+        <button onClick={() => display('1')}>1</button>
+        <button onClick={() => display('2')}>2</button>
+        <button onClick={() => display('3')}>3</button>
+        <button onClick={() => display('+')}>+</button>
+        <button onClick={() => display('.')}>.</button>
+        <button onClick={() => display('0')}>0</button>
+        <button onClick={calculate} className="equal">
+          =
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
